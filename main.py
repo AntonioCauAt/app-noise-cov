b94233190993ef5b6a533d21a190bb7d0d129203
AntonioCauAt <antoniocaulinatienzar@gmail.com>
2024-08-14 12:26:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,15 @@ __location__ = os.path.realpath(
 # Populate mne_config.py file with brainlife config.json
 with open(__location__+'/config.json') as config_json:
     config = json.load(config_json)
-    
-# Read the meg file
-epochs_fname = config.pop('fname')  
-# Read in the epochs file
-epo = mne.read_epochs(epochs_fname) 
-# since we computed our forward model for MEG only, we drop the EEG channels
-# MEEG configuration for true and false
+
+# Read the epochs file: Lee el archivo de epochs usando mne
+epochs_fname = config.pop('fname') 
+epo = mne.read_epochs(epochs_fname)  
+
+# Configuration depending on what we want
 epo.pick_types(meg=True, eeg=False)
 
-# Compute noise covariance matrix
+# Compute noise covariance matrix: Calcula la matriz de covarianza de ruido
 noise_cov = mne.compute_covariance(epo, tmax=0.,
                                    method=['shrunk', 'empirical'],
                                    rank='info')
@@ -31,18 +30,19 @@ print(noise_cov['method'])
 
 # == SAVE RESULTS ==
 
-# Opcional: si tienes una solución forward, guárdala
-# fwd_fname = os.path.join('out_dir', 'fwd.fif')
-# mne.write_forward_solution(fwd_fname, fwd, overwrite=True)
-
-# SAVE FIGURE
+# SAVE FIGURE: Create and save a fig of the covariance matrix
 fig = mne.viz.plot_cov(noise_cov, epo.info)
-fig_fname = os.path.join('out_dir', 'noise_cov.png')
+fig_fname = os.path.join('out__figs', 'noise_cov.png')
 fig.savefig(fig_fname)
 
-# SAVE REPORT
+# SAVE REPORT: Create and save a report that includes the previous figure
 report = mne.Report(title='Report')
 report.add_figs_to_section(fig, 'Noise Covariance', section='Covariance')
-report_path = os.path.join('out_dir_report', 'report.html')
+report_path = os.path.join('out__dir_report', 'report.html')
 report.save(report_path, overwrite=True)
+
+# Guardar los datos evocados
+#evoked_fname = os.path.join('out__dir', 'evoked-ave.fif')
+#evoked.save(evoked_fname)
+
 
