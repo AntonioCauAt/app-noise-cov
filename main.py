034ee92c990be0a31fd323f22ab2ba74b19306b5
AntonioCauAt <antoniocaulinatienzar@gmail.com>
2024-09-04 10:41:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,8 @@
 import mne
 import json
 import os
-import os.path as op
-import matplotlib.pyplot as plt
-from pathlib import Path
-import tempfile
 import numpy as np
 import matplotlib.pyplot as plt
-import sys
 
 #workaround for -- _tkinter.TclError: invalid command name ".!canvas"
 import matplotlib
@@ -24,14 +19,14 @@ with open('config.json') as config_json:
     config = json.load(config_json)
 
 # Read the epochs file
-epochs_fname = config.pop('fname') 
-epo = mne.read_epochs(epochs_fname)  
+fname = config ['epo']
+epochs = mne.read_epochs(fname)  
 
 # Configuration depending on what we want
-epo.pick_types(meg=True, eeg=False)
+epochs.pick_types(meg=True, eeg=False)
 
 # Compute noise covariance matrix
-noise_cov = mne.compute_covariance(epo, tmax=0.,
+noise_cov = mne.compute_covariance(epochs, tmax=0.,
                                    method=['shrunk', 'empirical'],
                                    rank='info')
 print(noise_cov['method'])
@@ -42,14 +37,16 @@ print(noise_cov['method'])
 cov_fname = os.path.join('out_dir', 'cov.fif')
 mne.write_cov(cov_fname, noise_cov)
 
+'''
 # SAVE FIGURE: Create and save a fig of the covariance matrix
-fig = mne.viz.plot_cov(noise_cov, epo.info)
+fig = mne.viz.plot_cov(noise_cov, epochs.info)
 fig_fname = os.path.join('out__figs', 'noise_cov.png')
 fig.savefig(fig_fname)
+'''
 
 # SAVE REPORT: Create and save a report that includes the previous figure
 report = mne.Report(title='Report')
-report.add_figs_to_section(fig, 'Noise Covariance', section='Covariance')
+#report.add_figs_to_section(fig, 'Noise Covariance', section='Covariance')
 report_path = os.path.join('out__dir_report', 'report.html')
 report.save(report_path, overwrite=True)
 
